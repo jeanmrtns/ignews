b4bbe9557e4653459232dbe97aa438248aa28506
Jean Martins <jeanmrtns4@gmail.com>
2022-05-15 13:17:36 -0300

    Prismic.io posts fetch

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,31 @@
 import {AppProps} from "next/app";
 import { SessionProvider as NextAuthProvider } from "next-auth/react";
+import Link from 'next/link'
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import { linkResolver, repositoryName } from '../../prismicio'
 import "../styles/global.scss";
 import {Header} from "../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <NextAuthProvider session={pageProps.session}>
-        <Header />
-        <Component {...pageProps} />
-      </NextAuthProvider>
+    <PrismicProvider
+        linkResolver={linkResolver}
+        internalLinkComponent={({ href, children, ...props }) => (
+            <Link href={href}>
+                <a {...props}>
+                    {children}
+                </a>
+            </Link>
+        )}
+    >
+        <PrismicPreview repositoryName={repositoryName}>
+            <NextAuthProvider session={pageProps.session}>
+                <Header />
+                <Component {...pageProps} />
+            </NextAuthProvider>
+        </PrismicPreview>
+    </PrismicProvider>
   )
 }
 
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,24 @@
 import Head from "next/head";
+
+import {GetStaticProps} from "next";
+import { RichText } from "prismic-dom";
+import { createClient } from '../../../prismicio';
+
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+interface Post {
+    slug: string;
+    first_publication_date: string;
+    title: string;
+    excerpt: string;
+}
+
+interface PostsProps {
+    posts: Post[]
+}
+
+export default function Posts({posts}: PostsProps) {
+    console.log(posts)
     return (
         <>
             <Head>
@@ -10,23 +27,48 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-                        <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
-                    </a>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-                        <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
-                    </a>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-                        <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
-                    </a>
+                    {
+                        posts && posts.map(post => {
+                            return (
+                                <a href={`http://localhost:3000/posts/${post.slug}`} key={post.slug}>
+                                    <time>{post.first_publication_date}</time>
+                                    <strong>{post.title}</strong>
+                                    <p>{post.excerpt || ''}</p>
+                                </a>
+                            )
+                        })
+                    }
                 </div>
             </main>
         </>
     )
+}
+
+export const getStaticProps: GetStaticProps = async ({ previewData }) => {
+    const client = createClient({ previewData })
+
+    const page = await client.getAllByType('post', {
+            fetch: ['post.title', 'post.content'],
+            pageSize: 100
+        }
+    );
+
+    const posts = page.map(post => {
+       return {
+           first_publication_date: new Intl.DateTimeFormat('pt-BR', {
+               year: 'numeric',
+               month: 'long',
+               day: '2-digit'
+           }).format(new Date(post.first_publication_date)),
+           title: RichText.asText(post.data.title),
+           excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+           slug: post.uid,
+       }
+    });
+
+    return {
+        props: {
+            posts
+        }, // Will be passed to the page component as props
+    }
 }
