e4c4f0517e09a853f07d16c929e7cef7ec125224
Jean Martins <jeanmrtns4@gmail.com>
2022-05-14 22:30:07 -0300

    Create Stripe session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,30 @@
 import styles from "./styles.module.scss";
+import {getStripe} from "../../services/stripejs.service";
+import {api} from "../../services/api.service";
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+
+    async function handleSubscribe() {
+        try {
+            const response = await api.post('/subscribe');
+            const { sessionId } = response.data;
+
+            const stripe = await getStripe();
+            await stripe.redirectToCheckout({ sessionId });
+        } catch (e) {
+            alert(e.message);
+        }
+    }
+
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Inscreva-se agora
         </button>
